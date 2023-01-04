2d6e5017c8ea8ba02348c87cbe46ee294f515d33
codeyourwaytofreedom <can.kolej@gmail.com>
2023-01-04 20:21:45 +0300

    Textarea bug solved.
    Will use a dynamic div instead.
    
    PDF saving successfull.
    
    Will keep modfying the CV details section.
    
    Good progress today.
    
    Keep going.
    
    Remmeber why you started.

--- components/details.tsx
+++ components/details.tsx
@@ -1,29 +1,39 @@
+import { faChevronCircleUp,faChevronCircleDown } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useState } from "react";
 import x from "../styles/cv.module.css";
 import Pen from "./pen";
 
 
 const Details = () => {
-    const [personal, setPersonal] = useState(["16px", 300])
-    const [personal_details, setPersonal_details] = useState(["16px", 300])
+    const [personal, setPersonal] = useState(["16px", 300, "none"])
+    const [personal_details, setPersonal_details] = useState(["16px", 300,])
+    const [textareaHeight, setTHeight] = useState(200)
 
     return ( 
         <div className={x.cv_content_details}>
             <div className={x.cv_content_details_chapter}>
                 <div className={x.cv_content_details_chapter_title}>
-                    <input type="text" placeholder="Personal Statement" style={{fontSize: personal[0], fontWeight: personal[1]}}/>
+                    <input type="text" placeholder="Personal Statement" style={{fontSize: personal[0], fontWeight: personal[1], textDecoration: personal[2]}}/>
                 </div>
                 <div className={x.cv_content_details_chapter_content}>
-                    <textarea name="eeee" spellCheck={false} style={{height:"200px", fontSize: personal_details[0], fontWeight: personal_details[1]}}></textarea>
+                    <textarea name="eeee" rows={30} spellCheck={false} style={{height:`${textareaHeight}px`, fontSize: personal_details[0], fontWeight: personal_details[1]}}></textarea>
+                </div>
+                <div contentEditable={true}>
+                    Hello
                 </div>
             </div>
 
             <div className={x.cv_content_details_tools}>
-                <Pen cls={x.cv_content_details_tools_pen} setter={setPersonal} value={personal} />
+                <Pen cls={x.cv_content_details_tools_pen} setter={setPersonal} value={personal} und={true} />
                 <br />
                 <br />
 
                 <Pen cls={x.cv_content_details_tools_pen} setter={setPersonal_details} value={personal_details} />
+                <div className={x.cv_content_details_tools_pen}>
+                        <FontAwesomeIcon icon={faChevronCircleUp} size={"2x"} onClick={()=> setTHeight(textareaHeight-5)}/>
+                        <FontAwesomeIcon icon={faChevronCircleDown} size={"2x"} onClick={()=> setTHeight(textareaHeight+5)}/>
+                </div>
             </div>
         </div>
      );
--- components/pen.tsx
+++ components/pen.tsx
@@ -1,21 +1,29 @@
-const Pen = ({cls, setter, value}) => {
+const Pen = ({cls, setter, value, und}) => {
     return ( 
             <div className={cls}>
-                    <button onClick={()=> setter(["16px", value[1]])}>
+                    <button onClick={()=> setter(["16px", value[1],value[2]])}>
                         16px
                     </button>
-                    <button onClick={()=> setter(["18px", value[1]])}>
+                    <button onClick={()=> setter(["18px", value[1],value[2]])}>
                         <span>18px</span>
                     </button>
-                    <button onClick={()=> setter(["20px", value[1]])}>
+                    <button onClick={()=> setter(["20px", value[1],value[2]])}>
                         <span>20px</span>
                     </button>
-                    <button onClick={()=> setter([value[0], 300])}>
+                    <button onClick={()=> setter([value[0], 300,value[2]])}>
                         <span>Light</span>
                     </button>
-                    <button onClick={()=> setter([value[0], 400])}>
+                    <button onClick={()=> setter([value[0], 400,value[2]])}>
                         <span>Bold</span>
                     </button>
+                    {
+                        und ? 
+                        <button onClick={()=> value[2] === "none" ? setter([value[0], value[1], "underline"]) :setter([value[0], value[1], "none"]) }>
+                            <span>Underline</span>
+                        </button>
+                        : null
+                    }
+                    
             </div>
      );
 }
