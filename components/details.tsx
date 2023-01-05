ef7bf06b434aab55fd1fa63da007028040f24105
codeyourwaytofreedom <can.kolej@gmail.com>
2023-01-05 16:38:26 +0300

    Education chapter added.
    Will be converted into a seperate component.
    
    that receives pen as props.
    
    Slow but safe and sound progress today.
    
    Keep going.
    
    Remember why you started.

--- components/details.tsx
+++ components/details.tsx
@@ -1,6 +1,6 @@
 import { faChevronCircleUp,faChevronCircleDown } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import x from "../styles/cv.module.css";
 import Pen from "./pen";
 
@@ -8,7 +8,9 @@ import Pen from "./pen";
 const Details = () => {
     const [personal, setPersonal] = useState(["16px", 300, "none"])
     const [personal_details, setPersonal_details] = useState(["16px", 300,])
-    const [textareaHeight, setTHeight] = useState(200)
+    const [education, setEducation] = useState(["16px", 300, "none"])
+
+    const test = useRef();
 
     return ( 
         <div className={x.cv_content_details}>
@@ -17,23 +19,77 @@ const Details = () => {
                     <input type="text" placeholder="Personal Statement" style={{fontSize: personal[0], fontWeight: personal[1], textDecoration: personal[2]}}/>
                 </div>
                 <div className={x.cv_content_details_chapter_content}>
-                    <textarea name="eeee" rows={30} spellCheck={false} style={{height:`${textareaHeight}px`, fontSize: personal_details[0], fontWeight: personal_details[1]}}></textarea>
+                    <div id={x.editable} contentEditable={true} ref={test} spellCheck={false} style={{ width:"500px",
+                            fontSize: personal_details[0], fontWeight: personal_details[1], fontFamily:"Oswald" }}
+                            onSelect={()=> 
+                                {
+                                    console.log(window.getSelection().toString());
+                                }
+                            }
+                            onKeyDown={()=> console.log("new letter detected")}
+                            >        
+                    </div>
                 </div>
-                <div contentEditable={true}>
-                    Hello
+                
+            </div>
+
+            <div className={x.cv_content_details_chapter}>
+                <div className={x.cv_content_details_chapter_title}>
+                    <input type="text" placeholder="Education" style={{fontSize: education[0], fontWeight: education[1], textDecoration: education[2]}}/>
+                    <div className={x.cv_content_details_chapter_content_controls}>
+                        <Pen cls={x.cv_content_details_tools_pen} setter={setPersonal} value={personal} und={true} />
+                    </div>
                 </div>
+                <div className={x.cv_content_details_chapter_content}>
+                    <div id={x.education_double}>
+                        <div id={x.editable} contentEditable={true} style={{width:"90px"}}>
+                            Year-Year
+                        </div>
+                        <div id={x.editable} contentEditable={true} ref={test} spellCheck={false} style={{
+                                fontSize: education[0], fontWeight: education[1], fontFamily:"Oswald", width:"400px" }}
+                        >        
+                                Heelo
+                        </div>
+                    </div>
+                    <div className={x.cv_content_details_chapter_content_controls}>
+                        <Pen cls={x.cv_content_details_tools_pen} setter={setPersonal} value={personal} und={true} />
+                    </div>
+                </div>
+                <div className={x.cv_content_details_chapter_content}>
+                    <div id={x.education_double}>
+                        <div id={x.editable} contentEditable={true} style={{width:"90px"}}>
+                            Year-Year
+                        </div>
+                        <div id={x.editable} contentEditable={true} ref={test} spellCheck={false} style={{
+                                fontSize: education[0], fontWeight: education[1], fontFamily:"Oswald", width:"400px" }}
+                        >        
+                                Faculty - Institution - etc
+                        </div>
+                    </div>
+                </div>
+                <div className={x.cv_content_details_chapter_content}>
+                    <div id={x.education_double}>
+                        <div id={x.editable} contentEditable={true} style={{width:"90px"}}>
+                            Year-Year
+                        </div>
+                        <div id={x.editable} contentEditable={true} ref={test} spellCheck={false} style={{
+                                fontSize: education[0], fontWeight: education[1], fontFamily:"Oswald", width:"400px" }}
+                        >        
+                                Faculty - Institution - etc
+                        </div>
+                    </div>
+                </div>
+                
             </div>
 
             <div className={x.cv_content_details_tools}>
                 <Pen cls={x.cv_content_details_tools_pen} setter={setPersonal} value={personal} und={true} />
-                <br />
-                <br />
 
                 <Pen cls={x.cv_content_details_tools_pen} setter={setPersonal_details} value={personal_details} />
-                <div className={x.cv_content_details_tools_pen}>
+{/*                 <div className={x.cv_content_details_tools_pen}>
                         <FontAwesomeIcon icon={faChevronCircleUp} size={"2x"} onClick={()=> setTHeight(textareaHeight-5)}/>
                         <FontAwesomeIcon icon={faChevronCircleDown} size={"2x"} onClick={()=> setTHeight(textareaHeight+5)}/>
-                </div>
+                </div> */}
             </div>
         </div>
      );
