a0f2b8f9097ebcc4603fb973a58d83b3e937fd53
codeyourwaytofreedom <can.kolej@gmail.com>
2023-01-06 15:12:40 +0300

    Code to be super simplified.
    Chapter conmponent created.
    
    Receives two pens for styling title and content.
    
    Will make the user experience very class.
    
    Keep going.
    
    Remember why you started.

--- components/chapter.tsx
+++ components/chapter.tsx
@@ -1,16 +1,28 @@
 import x from "../styles/cv.module.css";
+import { useRef, useState } from "react";
+import Pen from "./pen";
 
 
 const Chapter = () => {
+    const [title_Styles, setTitle_Styes] = useState(["16px", 300, "none"])
+    const [stly, setStly] = useState(["16px", 300, "none"])
     return ( 
         <div className={x.cv_content_details_chapter}>
                 <div className={x.cv_content_details_chapter_title}>
-                    <input type="text" placeholder="Chapter Title"/>
+                    <input type="text" placeholder="Chapter Title" style={{
+                        fontSize: title_Styles[0], fontWeight:title_Styles[1], textDecoration:title_Styles[2], border:"1px solid silver"
+                    }}/>
                 </div>
                 <div className={x.cv_content_details_chapter_content}>
-                    <div id={x.editable} contentEditable={true} spellCheck={false} style={{ width:"500px"}}>    
+                    <div id={x.editable} contentEditable={true} spellCheck={false} style={{ width:"500px",
+                        fontSize: stly[0], fontWeight:stly[1], textDecoration:stly[2], border:"1px solid silver"
+                        }}>    
                         Chapter Details
                     </div>
+                </div>
+                <div className={x.cv_content_details_chapter_content_controls}>
+                        <Pen cls={x.cv_content_details_tools_pen} setter={setTitle_Styes} value={title_Styles} und={true} />
+                        <Pen cls={x.cv_content_details_tools_pen} setter={setStly} value={stly} und={true} />
                 </div>
             </div>
      );
