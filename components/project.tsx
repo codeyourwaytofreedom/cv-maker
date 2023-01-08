f4e64b82467f64ad8b234b9ffbf3fbbaed256be2
codeyourwaytofreedom <can.kolej@gmail.com>
2023-01-08 13:53:23 +0300

    Project component done...
    CV general structure done.
    
    might add other components for chapters with different styles.
    
    Keep going.
    
    Remember why you started.

--- components/project.tsx
+++ components/project.tsx
@@ -9,10 +9,7 @@ const Project = () => {
     const [title_Styles, setTitle_Styes] = useState(["16px", 300, "none"]);
     const [stly, setStly] = useState(["16px", 300, "none"]);
     const [tools, setTools] = useState(null);
-    const [lines, setLines] = useState(2);
     const chapter_area = useRef();
-    const [color, setColor] = useState("green");
-    const [skills, setSkills] = useState(1);
 
     useEffect(()=> {
         const handle_outside_click = (event) => {
@@ -27,34 +24,50 @@ const Project = () => {
         window.document.addEventListener("click", handle_outside_click)
     },[])
     return ( 
-        <div className={x.cv_content_details_chapter} ref={chapter_area} style={{borderBottom:"none"}}>
+        <div className={x.cv_content_details_chapter} ref={chapter_area} style={{borderBottom:"none", paddingBottom:"0"}}>
             <div>
                 {/* Control Panel */}
                 <div className={x.cv_content_details_chapter_content}>   
                         {
-                            tools &&
+                            tools && tools === "content" &&
                             <div className={x.cv_content_details_chapter_content_controls} id={x.software_controls}>
 
                                         <h3>Title Styles</h3>
-                                        <Pen cls={x.cv_content_details_tools_pen} setter={setStly} value={stly} und={true} />
+                                        <Pen cls={x.cv_content_details_tools_pen} setter={setTitle_Styes} value={title_Styles} und={true} />
                             </div>
-           
                         }
                 </div>
 
+                <div className={x.cv_content_details_chapter_content}>   
+                    <span style={{ fontSize: stly[0]}}>
+                        <FontAwesomeIcon icon={faLink} color={"darkgreen"}/>
+                    </span>
+                    <div id={x.editable} contentEditable={true} spellCheck={false} onFocus={()=> setTools("content")} style={{ width:"500px",
+                        fontSize: title_Styles[0], fontWeight:title_Styles[1], textDecoration:title_Styles[2]
+                        }}>    
+                        Project Name
+                    </div>
+                </div>
 
-                        <div className={x.cv_content_details_chapter_content}>   
-                            <span style={{ fontSize: stly[0]}}>
-                                <FontAwesomeIcon icon={faLink} color={"darkgreen"}/>
-                            </span>
-                            <div id={x.editable} contentEditable={true} spellCheck={false} onFocus={()=> setTools("content")} style={{ width:"500px",
-                                fontSize: stly[0], fontWeight:stly[1], textDecoration:stly[2]
-                                }}>    
-                                Project Name
-                            </div>
-                        </div>
             </div>
-            <Explain/>
+
+            <div  ref={chapter_area}>
+                {/* Control Panel */}
+                <div className={x.cv_content_details_chapter_content}>   
+                    <div id={x.editable} contentEditable={true} spellCheck={false} onFocus={()=> setTools("project_detail")} style={{ width:"500px",
+                        fontSize: stly[0], fontWeight:stly[1], textDecoration:stly[2]
+                        }}>    
+                        Details
+                    </div>
+                        {
+                            tools && tools === "project_detail" &&
+                            <div className={x.cv_content_details_chapter_content_controls} id={x.software_controls}>
+                                <h3>Detail Styles</h3>
+                                <Pen cls={x.cv_content_details_tools_pen} setter={setStly} value={stly} und={false} />
+                            </div>
+                        }
+                </div>
+            </div>
         </div>
      );
 }
